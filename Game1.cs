a534194a284e81a99f616333bbae814472e6aa9c
Lucas A <luan01001@utb.vaxjo.se>
2019-04-05 10:45:03 +0200

    test

--- Game1.cs
+++ Game1.cs
@@ -4,27 +4,23 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Breakout_spel_Lucas_A_slutprojekt
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
-    /// testtastdat
+ 
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1080;
+            graphics.PreferredBackBufferHeight = 720;
             Content.RootDirectory = "Content";
+
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+       
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -32,16 +28,13 @@ namespace Breakout_spel_Lucas_A_slutprojekt
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+     
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
+          
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+           
+          
         }
 
         /// <summary>
@@ -74,9 +67,8 @@ namespace Breakout_spel_Lucas_A_slutprojekt
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
+            GraphicsDevice.Clear(Color.Purple);
+           
 
             base.Draw(gameTime);
         }
